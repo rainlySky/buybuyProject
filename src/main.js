b4980929a4712ee6d972b6891e4909e9854736e0
江雨雪 <1151930850@qq.com>
2018-07-25 21:11:34 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueRouter from 'vue-router'
+import index from './components/index.vue';
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+const router = new VueRouter({
+  routes:
+    [
+      { path: '/', component: index },
+      { path: '/index', component: index }
+  ]
+})
+
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
+
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  el:'#app',
+  render: h => h(App),
+  router,
+})
